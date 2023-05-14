8cfc22a351aab8d650c439b515c8aed6578ae9cd
Daniel Khashev (Angelhe1m) <116936326+angelhe1m@users.noreply.github.com>
2023-05-14 23:46:23 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,45 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() {
+  countOp = 0;
+  first = nullptr;
+}
+
+void Train::addCage(bool light) {
+  Cage* mashine = new Cage();
+  mashine->light = light;
+  if (first == nullptr) {
+    first = mashine;
+    first->next = mashine;
+    first->prev = mashine;
+  } else {
+    mashine->next = first;
+    mashine->prev = first->prev;
+    (first->prev)->next = mashine;
+    first->prev = mashine;
+  }
+}
+
+int Train::getLength() {
+  int length = 0;
+  first->light = true;
+  while (first->light) {
+    length = 0;
+    Cage* cur_mashine = first->next;
+    countOp++;
+    length++;
+    while (!cur_mashine->light) {
+      countOp++;
+      length++;
+      cur_mashine = cur_mashine->next;
+    }
+    cur_mashine->light = false;
+  }
+  countOp *= 2;
+  return length;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
